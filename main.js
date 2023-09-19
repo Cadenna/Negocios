3f5921d41eec1c35bbabc69fa7cedd2d6bf418d3
cadena <cadenamartinez@gmail.com>
2023-09-18 18:02:24 -0600

    read-create

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//validation form
 function validateForm(){
 
     let name = document.getElementById('inputName').value;
@@ -14,4 +15,61 @@ function validateForm(){
     }
 
     return true;
+}
+
+//read
+function showData(){
+
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem("listPeople"));
+    }
+
+    var html = "";
+
+    listPeople.forEach(function(element, index){
+        html += "<tr>";
+        html += '<td class="col-md-1"; style="text-align: center"><button onclick="updateData('+ index +')" class="btn btn-outline-dark"><i class="bi bi-pencil-square"></i></i></button></td>';
+        html += '<td class="col-md-5">' + element.name + '</td>';
+        html += '<td class="col-md-3">' + element.date + '</td>';
+        html += '<td class="col-md-1"; style="text-align: center"><button onclick="deleteData('+ index +')" class="btn btn-outline-dark"><i class="bi bi-x-lg"></i></button></td>';
+        html += "</tr>";
+    });
+
+    document.querySelector('#tableData tbody').innerHTML = html;
+}
+
+//create
+document.onload = showData();
+
+function AddData(){
+    if (validateForm() == true) {
+        
+        let name = document.getElementById('inputName').value;
+        let date = document.getElementById('inputDate').value;
+
+        var listPeople;
+        if (localStorage.getItem('listPeople') == null) {
+            listPeople = [];
+        }else{
+            listPeople = JSON.parse(localStorage.getItem("listPeople"));
+        }
+
+        listPeople.push({
+           
+            name: name,
+            date: date,
+        });
+
+        localStorage.setItem('listPeople', JSON.stringify(listPeople));
+
+        showData();
+
+       
+        document.getElementById('inputName').value = "";
+        document.getElementById('inputDate').value = "";
+    }
 }
